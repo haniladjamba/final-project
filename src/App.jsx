75327d95d4484831f863fec2b09ed47be2977878
haniladjamba <s22110313@student.unklab.ac.id>
2023-11-22 23:02:54 +0800

    navbar not showing in full screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,160 @@
-import './App.css'
+import "./assets/style.css";
 
 function App() {
   return (
-  )
+    <body>
+      <div className="wrapper d-flex align-items-stretch">
+        <nav id="sidebar" className="active">
+          <div className="p-4 pt-5">
+            <a
+              href="#"
+              className="img logo rounded-circle mb-5"
+              style={{ backgroundImage: "url(images/logo.jpg)" }}
+            />
+            <ul className="list-unstyled components mb-5">
+              <li className="active">
+                <a
+                  href="#homeSubmenu"
+                  data-toggle="collapse"
+                  aria-expanded="false"
+                  className="dropdown-toggle"
+                >
+                  Home
+                </a>
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                  <li>
+                    <a href="#">Home 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Home 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Home 3</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a
+                  href="#pageSubmenu"
+                  data-toggle="collapse"
+                  aria-expanded="false"
+                  className="dropdown-toggle collapsed"
+                >
+                  Pages
+                </a>
+                <ul
+                  className="list-unstyled collapse"
+                  id="pageSubmenu"
+                  style={{}}
+                >
+                  <li>
+                    <a href="#">Page 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Page 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Page 3</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+            <div className="footer">
+              <p>
+                Copyright © 2023 All rights reserved | This template is made
+                with
+                <i className="icon-heart" aria-hidden="true" /> by
+                <a href="#" target="_blank">
+                  Colorlib.com
+                </a>
+              </p>
+            </div>
+          </div>
+        </nav>
+        <div id="content" className="p-4 p-md-5">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <button
+                type="button"
+                id="sidebarCollapse"
+                className="btn btn-primary"
+              >
+                <i className="fa fa-bars" />
+                <span className="sr-only">Toggle Menu</span>
+              </button>
+              <button
+                className="btn btn-dark d-inline-block d-lg-none ml-auto collapsed"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i className="fa fa-bars" />
+              </button>
+              <div
+                className="navbar-collapse collapse"
+                id="navbarSupportedContent"
+                style={{}}
+              >
+                <ul className="nav navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Portfolio
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <h2 className="mb-4">Sidebar #01</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </body>
+  );
 }
 
-export default App
+export default App;
