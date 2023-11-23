48416a486a89a61461f7488e9e21704cae370a3a
nmakatipu-16 <nogemanyombexxx@gmail.com>
2023-11-23 22:30:14 +0800

    modify add pertama

--- src/App.jsx
+++ src/App.jsx
@@ -143,82 +143,51 @@ function App() {
             </div>
           </nav>
           <h2 className="mb-4">Sidebar #01</h2>
-          {/*For Page*/}
-          <div className="page">
-            {/*Card*/}
-            <div className="card">
-              {/*Card Header*/}
-              <div className="card-header">
-                <p>
-                  {" "}
-                  <i className="fa fa-bars" />{" "}
-                  <i className="fa fa-calendar ml-4" aria-hidden="true" />{" "}
-                  <i className="fa fa-star ml-4" aria-hidden="true" />{" "}
-                  <span className="float-right">
-                    {" "}
-                    <span className="mr-4 navTask">Task</span>{" "}
-                    <span className="mr-4">Archive</span>{" "}
-                    <i className="fa fa-search" aria-hidden="true" />{" "}
-                  </span>{" "}
-                </p>
-              </div>
-              {/*Card Body*/}
-              <div className="card-body">
-                <p className="heading1">
-                  {" "}
-                  <span className="today">Today</span>{" "}
-                  <span className="float-right headingright">7h 15min</span>{" "}
-                </p>
-                <p>
-                  {" "}
-                  <i className="fa fa-calendar mr-2" aria-hidden="true" />{" "}
-                  <span className="task mt-4">Take kids to school</span>{" "}
-                  <span className="time ml-2">8:00-8:30AM</span>{" "}
-                  <span className="float-right">30min</span>{" "}
-                </p>
-                <p>
-                  <i className="	fa fa-circle-thin mr-2" />{" "}
-                  <span className="task">Process email</span>{" "}
-                  <i className="fa fa-thumb-tack ml-2" aria-hidden="true" />{" "}
-                  <span className="time ml-2">8:00-9:30AM</span>
-                  <span className="float-right">1h</span>{" "}
-                </p>
-                <p>
-                  <i className="fa fa-calendar mr-2" aria-hidden="true" />{" "}
-                  <span className="task">Daily Stand-Up meeting</span>{" "}
-                  <span className="time ml-2">9:00-10:00AM</span>{" "}
-                  <span className="float-right">30min</span>{" "}
-                </p>
-                <p>
-                  <i className="	fa fa-circle-thin mr-2" />
-                  <span className="task">Create new templates</span>{" "}
-                  <i className="fa fa-thumb-tack ml-2" aria-hidden="true" />{" "}
-                  <i className="fa fa-user ml-2" />{" "}
-                  <span className="time ml-2">10:00-11:45AM</span>{" "}
-                  <span className="float-right">1h 45min</span>{" "}
-                </p>
-                <p>
-                  <i className="fa fa-calendar mr-2" aria-hidden="true" />{" "}
-                  <span className="task">Lunch with Anna</span>{" "}
-                  <span className="time ml-2">12:00-12:30PM</span>{" "}
-                  <span className="float-right">30min</span>{" "}
-                </p>
-                <p className="text-muted">
-                  <i className="fa fa-plus mr-1" aria-hidden="true" /> Add Task
-                  for Today
-                </p>
-                <p className="heading2">
-                  <span className="tomorrow">Tomorrow</span>{" "}
-                  <span className="float-right headingright">6h 30min</span>{" "}
-                </p>
-                <p className="task2 mt-4">
-                  <i className="fa fa-calendar mr-2" aria-hidden="true" />{" "}
-                  <span className="task">
-                    Breakfast with the Marketing team
-                  </span>{" "}
-                  <span className="time ml-2">8:00-8:30AM</span>{" "}
-                  <span className="float-right">30min</span>
-                </p>
+          <div>
+            <meta charSet="UTF-8" />
+            <title>Task Priority</title>
+            <link rel="stylesheet" href="styles.css" />
+            <div className="page">
+              <div className="priority-box">
+                <div className="priority high">
+                  <div className="card">
+                    <div className="card-header">
+                      <h2>High Priority</h2>
+                    </div>
+                    <div className="card-body">
+                      {/* High priority tasks here */}
+                      <p>Task 1</p>
+                      <p>Task 2</p>
+                      {/* ... */}
+                    </div>
+                  </div>
+                </div>
+                <div className="priority medium">
+                  <div className="card">
+                    <div className="card-header">
+                      <h2>Medium Priority</h2>
+                    </div>
+                    <div className="card-body">
+                      {/* Medium priority tasks here */}
+                      <p>Task 3</p>
+                      <p>Task 4</p>
+                      {/* ... */}
+                    </div>
+                  </div>
+                </div>
+                <div className="priority low">
+                  <div className="card">
+                    <div className="card-header">
+                      <h2>Low Priority</h2>
+                    </div>
+                    <div className="card-body">
+                      {/* Low priority tasks here */}
+                      <p>Task 5</p>
+                      <p>Task 6</p>
+                      {/* ... */}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
