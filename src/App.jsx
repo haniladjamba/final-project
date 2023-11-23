9c971ab8122ad71686e36d332ae5f194b666422c
nmakatipu-16 <nogemanyombexxx@gmail.com>
2023-11-23 23:33:15 +0800

    add color

--- src/App.jsx
+++ src/App.jsx
@@ -147,45 +147,43 @@ function App() {
             <meta charSet="UTF-8" />
             <title>Task Priority</title>
             <link rel="stylesheet" href="styles.css" />
-            <div className="page">
-              <div className="priority-box">
-                <div className="priority high">
-                  <div className="card">
-                    <div className="card-header">
-                      <h2>High Priority</h2>
-                    </div>
-                    <div className="card-body">
-                      {/* High priority tasks here */}
-                      <p>Task 1</p>
-                      <p>Task 2</p>
-                      {/* ... */}
-                    </div>
+            <div className="priority-box">
+              <div className="priority high">
+                <div className="card">
+                  <div className="card-header">
+                    <h2>High Priority</h2>
+                  </div>
+                  <div className="card-body">
+                    {/* High priority tasks here */}
+                    <p>Task 1</p>
+                    <p>Task 2</p>
+                    {/* ... */}
                   </div>
                 </div>
-                <div className="priority medium">
-                  <div className="card">
-                    <div className="card-header">
-                      <h2>Medium Priority</h2>
-                    </div>
-                    <div className="card-body">
-                      {/* Medium priority tasks here */}
-                      <p>Task 3</p>
-                      <p>Task 4</p>
-                      {/* ... */}
-                    </div>
+              </div>
+              <div className="priority medium">
+                <div className="card">
+                  <div className="card-header">
+                    <h2>Medium Priority</h2>
+                  </div>
+                  <div className="card-body">
+                    {/* Medium priority tasks here */}
+                    <p>Task 3</p>
+                    <p>Task 4</p>
+                    {/* ... */}
                   </div>
                 </div>
-                <div className="priority low">
-                  <div className="card">
-                    <div className="card-header">
-                      <h2>Low Priority</h2>
-                    </div>
-                    <div className="card-body">
-                      {/* Low priority tasks here */}
-                      <p>Task 5</p>
-                      <p>Task 6</p>
-                      {/* ... */}
-                    </div>
+              </div>
+              <div className="priority low">
+                <div className="card">
+                  <div className="card-header">
+                    <h2>Low Priority</h2>
+                  </div>
+                  <div className="card-body">
+                    {/* Low priority tasks here */}
+                    <p>Task 5</p>
+                    <p>Task 6</p>
+                    {/* ... */}
                   </div>
                 </div>
               </div>
