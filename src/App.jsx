c47c1c83f62acc18f7f6bd3e7943a6e5ba7be13d
nmakatipu-16 <nogemanyombexxx@gmail.com>
2023-11-25 21:42:46 +0800

    add component task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./assets/style.css";
 import { useState } from "react";
 import FullHeightComponent from "./components/FullHeight";
+import Task from "./components/task";
 
 function App() {
   const [sidebarActive, setSidebarActive] = useState(false);
@@ -144,50 +145,7 @@ function App() {
           </nav>
           <h2 className="mb-4">Sidebar #01</h2>
           <div>
-            <meta charSet="UTF-8" />
-            <title>Task Priority</title>
-            <link rel="stylesheet" href="styles.css" />
-            <div className="priority-box justify-content-center d-flex">
-              <div className="priority high">
-                <div className="card">
-                  <div className="card-header">
-                    <h2>High Priority</h2>
-                  </div>
-                  <div className="card-body">
-                    {/* High priority tasks here */}
-                    <p>Task 1</p>
-                    <p>Task 2</p>
-                    {/* ... */}
-                  </div>
-                </div>
-              </div>
-              <div className="priority medium">
-                <div className="card">
-                  <div className="card-header">
-                    <h2>Medium Priority</h2>
-                  </div>
-                  <div className="card-body">
-                    {/* Medium priority tasks here */}
-                    <p>Task 3</p>
-                    <p>Task 4</p>
-                    {/* ... */}
-                  </div>
-                </div>
-              </div>
-              <div className="priority low">
-                <div className="card">
-                  <div className="card-header">
-                    <h2>Low Priority</h2>
-                  </div>
-                  <div className="card-body">
-                    {/* Low priority tasks here */}
-                    <p>Task 5</p>
-                    <p>Task 6</p>
-                    {/* ... */}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Task/>
           </div>
         </div>
       </div>
