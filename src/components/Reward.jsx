8e5d202a0da4a14b8d9582250ce322623e9aa45e
haniladjamba <s22110313@student.unklab.ac.id>
2023-12-02 19:09:24 +0800

    get all data from all categories and activite claim button to emove data base on IDs

--- src/components/Reward.jsx
+++ src/components/Reward.jsx
@@ -1,82 +1,104 @@
 import React, { useEffect, useState } from 'react';
 import './Reward.css';
 import propTypes from 'prop-types';
-import { getDatabase, ref, get } from 'firebase/database';
+import { getDatabase, ref, get, remove, onValue } from 'firebase/database';
 
 const Reward = () => {
-  
-  // const [rewardState, setRewards] = useState(newObject);
-
-  // console.log(rewards);
-
-  // if (!rewards || !Array.isArray(rewards) || rewards.length === 0) {
-  //   return <p>Loading rewards...</p>;
-  // }
-  const [rewards, setRewards] = useState([{ priority: '', reward: '', todo: '' }]);
+  const [rewards, setRewards] = useState([
+    { priority: "", reward: "", todo: "" },
+  ]);
+  const [taskIDs, setTaskIDs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const db = getDatabase();
-
-        // Replace 'your-firebase-endpoint' with the actual endpoint where your data is stored
-        const dataRef = ref(db, 'task/other');
-        const snapshot = await get(dataRef);
-        const data = snapshot.val();
-
-        // Filter data where priority is 1
-        const todoPriority1 = Object.values(data)
-          .filter(item => item.priority === 1);
-
-        setRewards(todoPriority1);
+        const categories = ['other', 'today', 'tommorow'];
+        const allData = [];
+        const allIDs = [];
+  
+        for (const category of categories) {
+          const dataRef = ref(db, `task/${category}`);
+          const snapshot = await get(dataRef);
+          const data = snapshot.val();
+  
+          if (data) {
+            const categoryData = Object.entries(data).map(([id, item]) => ({
+              ...item,
+              id,
+              category,
+            }));
+            const categoryIDs = Object.keys(data);
+            allIDs.push(...categoryIDs);
+  
+            allData.push(...categoryData);
+          }
+        }
+  
+        // Assuming allIDs contains the unique IDs
+        console.log('All IDs:', allIDs);
+  
+        setRewards(allData);
+        setTaskIDs(allIDs);
       } catch (error) {
         console.error('Error fetching data from Firebase:', error);
       }
     };
-
+  
     fetchData();
   }, []);
-
-  rewards.id = 2
+  
 
   console.log(rewards);
 
+  const handleClaim = async (index) => {
+    try {
+      const db = getDatabase();
 
-const handleClaim = (index) => {
-    // Create a new array excluding the randomly chosen item
-    const updatedRewards = rewards.filter((_, i) => i !== index);
-    setRewards(updatedRewards);
-    // Additional logic: perform any other actions needed when claiming a reward
+      // Get the task ID and category based on the index
+      const { id, category } = rewards[index];
+
+      // Remove the task from the database using its unique ID
+      await remove(ref(db, `task/${category}/${id}`));
+
+      // Update the local state to reflect the removal
+      const updatedRewards = rewards.filter((_, i) => i !== index);
+      setRewards(updatedRewards);
+
+      // Additional logic: perform any other actions needed when claiming a reward
+    } catch (error) {
+      console.error("Error removing data from Firebase:", error);
+    }
   };
 
- return (
-  <div>
-  <p style={{color: "#B6EADA"}} className="lead">in here you can see all the reward you can get if you complete a task.
-    This can make you more motivated to do your task.
-    So the more you do complete your task, the more reward you can get.
+  return (
+    <div>
+      <p style={{ color: "#B6EADA" }} className="lead">
+        in here you can see all the reward you can get if you complete a task.
+        This can make you more motivated to do your task. So the more you do
+        complete your task, the more reward you can get.
+      </p>
+      <div className="reward-container">
+        <h2>My Rewards</h2>
 
-  </p>
-    <div className="reward-container">
-      <h2>My Rewards</h2>
-
-      {rewards.map((rewards, index) => (
-        <div className="reward-box" key={index}>
-          <h3>{rewards.reward}</h3>
-          <p>{rewards.todo}</p>
-          <div className="task-id">
-            Task ID: {rewards.priority}
-            <button
-              className="claim-button btn btn-success"
-              onClick={() => handleClaim(index)}
-            >
-              Claim
-            </button>
+        {rewards.map((rewards, index) => (
+          <div className="reward-box" key={index}>
+            <h3>{rewards.reward}</h3>
+            <p>{rewards.todo}</p>
+            <div className="task-id">
+              Task ID: {rewards.priority} ({rewards.category})
+              <button
+                className="claim-button btn btn-success"
+                onClick={() => handleClaim(index)}
+              >
+                Claim
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
- );
+  );
 };
 
 export default Reward;
--- src/components/database/GetPriority1.jsx
+++ src/components/database/GetPriority1.jsx
@@ -9,19 +9,33 @@ const GetPriority = ({ priority }) => {
     };
 
   const [todos, setTodos] = useState([]);
+  const [allData, setAllData] = useState([{ priority: '', reward: '', todo: '' }]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const db = getDatabase();
 
-        // Replace 'your-firebase-endpoint' with the actual endpoint where your data is stored
-        const dataRef = ref(db, 'task/other');
-        const snapshot = await get(dataRef);
-        const data = snapshot.val();
+         // Fetch data from 'other', 'today', and 'tomorrow'
+         const categories = ['other', 'today', 'tommorow'];
+         const allData = [];
+ 
+         for (const category of categories) {
+           const dataRef = ref(db, `task/${category}`);
+           const snapshot = await get(dataRef);
+           const data = snapshot.val();
+ 
+           // Extract data values and add them to the array
+           if (data) {
+             const categoryData = Object.values(data).map(item => ({ ...item, category }));
+             allData.push(...categoryData);
+           }
+         }
+
+        setAllData(allData);
 
         // Filter data where priority is 1
-        const priority1Todos = Object.values(data)
+        const priority1Todos = Object.values(allData)
           .filter(item => item.priority === priority);
 
         setTodos(priority1Todos);
@@ -37,7 +51,7 @@ const GetPriority = ({ priority }) => {
     <>
         {todos.map((todo, index) => (
           <li key={index}>
-            <strong>Todo:</strong> {todo.todo} | <strong>Reward:</strong> {todo.reward}
+            <strong>Todo:</strong> {todo.todo} | <strong>Reward:</strong> {todo.reward} | <strong>From:</strong> {todo.category}
           </li>
         ))}
     </>
